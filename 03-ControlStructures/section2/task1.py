9e7f000f2e8655c15081245e61514deb38b5a36a
MikolajOczos <236815@student.uek.krakow.pl>
2024-10-18 12:39:57 +0200

    xD

---
+++ 03-ControlStructures/section2/task1.py
@@ -0,0 +1,11 @@
+size = input("What is your clothing size?")
+             
+if  size== "S":
+    print("small size")
+elif  size=="M":
+    print("medium size")
+elif size=="L":
+    print("Large size")
+elif size=="XL":
+    print("Extra Large Size")
+ 
---
+++ 03-ControlStructures/section2/task5.py
@@ -0,0 +1,17 @@
+###
+# Calculates and prints the quarter of the year for a given
+# month number (1..12)
+#
+month = int(input('Enter month number (1..12): '))
+
+if month >= 10:
+    quarter = 4
+elif month <= 3:
+    quarter = 1
+elif month <= 6:
+    quarter = 2
+else:
+    quarter = 3
+    
+
+print(f'Month {month} is in quarter {quarter}')
